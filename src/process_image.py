7c0bea73c30e993838b0520e3ea1907612cca013
gorogoroyasu <gorogoro.yasu@gmail.com>
2019-05-05 00:57:15 +0900

    fixed some issues

--- src/process_image.py
+++ src/process_image.py
@@ -1,19 +1,27 @@
+import numpy as np
+from PIL import Image, ImageDraw
+
+
 class Processor:
-    def __init__(self, image, boxes, labels, internal, cut_size=(300, 300)):
-        self.image = image
+    def __init__(self, image, boxes, labels, internal, cut_size=(300, 300), split=True):
+        self.image = np.asarray(image)
         self.boxes = boxes
         self.labels = labels
         self.internal = internal
         self.cut_size = cut_size
-        self.cut_image()
-        self.cut_images = self.cut_image()
-        self.cut_bboxes, self.cut_labels = self.bboxes_and_labels()
-        self.cis, self.cbs, self.cls = self.drop_empty_pictures
+        self.split = split
+
+        self.cut_images, self.cut_bboxes, self.cut_labels = self.bboxes_and_labels()
+        self.cis, self.cbs, self.cls = self.drop_empty_pictures()
 
     @property
     def cut_image_with_info(self):
         for i, _ in enumerate(self.cis):
-            yield self.cis[i], self.cbs[i], self.cls[i]
+            image = Image.fromarray(self.cis[i])
+            for cbs in self.cbs[i]:
+                draw = ImageDraw.Draw(image)
+                draw.rectangle((cbs[0], cbs[2], cbs[1], cbs[3]), fill=False, outline=(255, 0, 0))
+            yield image, self.cbs[i], self.cls[i]
 
     def drop_empty_pictures(self):
         # cut images
@@ -22,37 +30,35 @@ class Processor:
         cbs = []
         # cut labels
         cls = []
-        assert len(self.cut_labels) == len(self.cut_bboxes) == len(self.cut_images)
         for i, b in enumerate(self.cut_labels):
-            if len(b) != 0:
-                cis.append(self.cut_images[i])
-                cbs.append(self.cut_bboxes[i])
-                cls.append(self.cut_labels[i])
+            cis.append(self.cut_images[b])
+            cbs.append(self.cut_bboxes[b])
+            cls.append(self.cut_labels[b])
+        assert len(cls) == len(cis) == len(cbs)
 
         return cis, cbs, cls
 
     def image_cut_rules(self):
         h, w, _ = self.image.shape
         cut_h, cut_w = self.cut_size
-
         for dh in range((h // cut_h) - 1):
             for dw in range((w // cut_w) - 1):
                 ch = dh * cut_h
                 cw = dw * cut_w
-                yield ch, ch + dh, cw, cw + dw
+                yield ch, ch + cut_h, cw, cw + cut_w
 
-    def cut_image(self):
         """
-        cut image
-        :return:
+        half slipped
         """
-        cut_img = dict()
-        for count, (ch, ch_next, cw, cw_next) in enumerate(self.image_cut_rules()):
-            if count not in cut_img.keys():
-                cut_img[count] = []
-            cut_img[count] = self.image[ch:ch_next, cw:cw_next]
+        begin_h = cut_h + cut_h // 2
+        begin_w = cut_w + cut_w // 2
 
-        return cut_img
+        if self.split:
+            for dh in range((h // cut_h) - 2):
+                for dw in range((w // cut_w) - 2):
+                    ch = dh * cut_h + begin_h
+                    cw = dw * cut_w + begin_w
+                    yield ch, ch + cut_h, cw, cw + cut_w
 
     def bboxes_and_labels(self):
         """
@@ -61,29 +67,46 @@ class Processor:
         :return:
         """
 
+        cut_img = dict()
         box = {}
         label = {}
-        for count, picture_coordinate in enumerate(self.image_cut_rules()):
-            for boxes, labels in zip(self.boxes, self.labels):
-                bx1, bx2, by1, by2 = boxes['x1'], boxes['x2'], boxes['y1'], boxes['y2']
+        for count, (ch, ch_next, cw, cw_next) in enumerate(self.image_cut_rules()):
 
-                if self.in_cur_picture(picture_coordinate, (bx1, bx2, by1, by2)):
+            if count not in cut_img.keys():
+                cut_img[count] = []
+            cut_img[count] = self.image[ch:ch_next, cw:cw_next]
+            c = Image.fromarray(cut_img[count])
+            c.save("./demo/imgs/{}.jpg".format(count))
+
+            for boxes, labels in zip(self.boxes, self.labels):
+                # boxes['x1'], boxes['x2'], boxes['y1'], boxes['y2']
+                bx1, bx2, by1, by2 = boxes[0], boxes[1], boxes[2], boxes[3]
+
+                """
+                initialize the position of box
+                """
+                if self.in_cur_picture((ch, ch_next, cw, cw_next), (bx1, bx2, by1, by2)):
                     if count not in box.keys():
                         box[count] = []
                         label[count] = []
+                    boxes[0] = bx1 - cw
+                    boxes[1] = bx2 - cw
+                    boxes[2] = by1 - ch
+                    boxes[3] = by2 - ch
                     box[count].append(boxes)
                     label[count].append(labels)
-
-        return box, label
+                    print(boxes, ch, cw)
+        return cut_img, box, label
 
     @staticmethod
     def between(p1, p2, target):
         return p1 <= target, p2
 
     def in_cur_picture(self, picture_position, bbox_coordinate):
-        px1, px2, py1, py2 = picture_position
+        # CAUTION: the coordinate for picture and bbox is not the same.
+        py1, py2, px1, px2 = picture_position
         bx1, bx2, by1, by2 = bbox_coordinate
-
+        print('ppos', picture_position, 'bpos', bbox_coordinate)
         if self.internal:
             """
             (px1, py1)
--- src/tlimm.py
+++ src/tlimm.py
@@ -1,13 +1,43 @@
 from pathlib import Path
+from process_image import Processor
+from reader import ImageAndAttributeReader
+from writer import ImageAndAttributeWriter
 
 
 class Cut:
-    def __init__(self, input_dir, output_dir, size, internal=True):
-        self.input_dir = input_dir
-        self.output_dir = output_dir
+    def __init__(self,
+                 image_input_dir='/Users/gorogoroyasu/dockerWorld/competitions/signate/159/data/train_images',
+                 annotation_input_dir='/Users/gorogoroyasu/dockerWorld/competitions/signate/159/data/train_annotations',
+                 image_output_dir='./demo/imgs',
+                 annotation_output_dir='./demo/anns',
+                 size=(300, 300),
+                 internal=True):
+        self.image_input_dir = image_input_dir
+        self.annotation_input_dir = annotation_input_dir
+        self.image_output_dir = image_output_dir
+        self.annotation_output_dir = annotation_output_dir
+
         self.size = size
         self.internal = internal
+        self.load_materials()
 
     def load_materials(self):
-        Path(self.input_dir).glob('*.json')
+        input_images = sorted(Path(self.image_input_dir).glob('*.jpg'))
+        input_annotations = sorted(Path(self.annotation_input_dir).glob('*.json'))
+        reader = ImageAndAttributeReader(input_images, input_annotations)
+        writer = ImageAndAttributeWriter(self.image_output_dir, self.annotation_output_dir)
 
+        for count, (image, boxes, labels) in enumerate(reader.parse()):
+            p = Processor(image, boxes, labels, internal=True, cut_size=self.size)
+            for count_in_image, (i, b, l) in enumerate(p.cut_image_with_info):
+                filename = '{}-{}'.format(count, count_in_image)
+                writer.write(i, b, l, filename)
+
+                if count_in_image > 30:
+                    break
+            break
+
+
+if __name__ == "__main__":
+    # size = (1000, 1000)
+    Cut()
