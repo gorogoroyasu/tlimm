9ee0d899327f5c6c527cdf59082b2bc5fad095a8
gorogoroyasu <gorogoro.yasu@gmail.com>
2019-05-06 14:34:11 +0900

    version incremented

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 requires = ['pillow', 'numpy']
 setup(
     name="tlimm",
-    version='0.13',
+    version='0.14',
     description='Image Cropping Library for Object Detection.',
     url='https://github.com/gorogoroyasu/tlimm',
     author='gorogoroyasu',
